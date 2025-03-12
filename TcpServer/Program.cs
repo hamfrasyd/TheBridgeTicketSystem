d55f3465080986086210ed341cd979bf7640f32e
Kevin Waweru <kew001@edu.zealand.dk>
2025-03-12 21:09:51 +0100

    TCP Client

---
+++ TcpServer/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TcpServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Server server = new Server(); // Du kan ændre porten her
+            server.Start();
+            Console.ReadKey();
+        }
+    }
+}
